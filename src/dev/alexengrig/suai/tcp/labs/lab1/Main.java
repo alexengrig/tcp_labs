411c408dbda6f1eda6acda9714536fe656d4700b
Grig Alex <alexengrigdev@gmail.com>
2020-09-15 21:29:39 +0300

    Fix lab 1

--- src/dev/alexengrig/suai/tcp/labs/lab1/Main.java
+++ src/dev/alexengrig/suai/tcp/labs/lab1/Main.java
@@ -2,17 +2,28 @@ package dev.alexengrig.suai.tcp.labs.lab1;
 
 public class Main {
     public static void main(String[] args) {
+        System.out.println("Iterative:");
         for (int number = 0; number < 10; number++) {
             int expected = 2 * number;
-            int actual = doubleUp(number);
-            System.out.println("2 * " + number + " = " + expected + "\t(" + (expected == actual) + ")");
+            System.out.println("2 * " + number + " = " + expected +
+                    "\t(" + (expected == doubleUpIterative(number)) + ")");
+        }
+        System.out.println("Recursive:");
+        for (int number = 0; number < 10; number++) {
+            int expected = 2 * number;
+            System.out.println("2 * " + number + " = " + expected +
+                    "\t(" + (expected == doubleUpRecursive(number)) + ")");
         }
     }
 
-    private static int doubleUp(int number) {
-        if (number == 0) {
-            return 0;
-        }
-        return 2 + doubleUp(number - 1);
+    private static int doubleUpIterative(int number) {
+        int result = 0;
+        for (int current = number; current > 0; current--) result += 2;
+        return result;
+    }
+
+    private static int doubleUpRecursive(int number) {
+        if (number == 0) return 0;
+        return 2 + doubleUpRecursive(number - 1);
     }
 }
